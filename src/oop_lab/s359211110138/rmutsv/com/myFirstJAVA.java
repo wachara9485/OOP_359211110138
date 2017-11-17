395ef1ac0a8d772d2c8b7780dfcdf0cb08d7b4e6
wachara9485 <wachara9485@gmail.com>
2017-11-17 10:36:29 +0700

    Initial commit

---
+++ src/oop_lab/s359211110138/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,9 @@
+package oop_lab.s359211110138.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.printIn("Hello Wordl, I'm mos");
+    }//main
+
+}//class
