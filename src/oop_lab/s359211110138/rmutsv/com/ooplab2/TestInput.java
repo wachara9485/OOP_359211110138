77592e6737d4bfa298c7afa7c04da8a3039eaad9
wachara9485 <wachara9485@gmail.com>
2017-11-24 10:42:35 +0700

    TestInput

---
+++ src/oop_lab/s359211110138/rmutsv/com/ooplab2/TestInput.java
@@ -0,0 +1,18 @@
+package oop_lab.s359211110138.rmutsv.com.ooplab2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestInput {
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.println("What is your name? :");
+        String name = reader.readLine();
+        System.out.println("Your name is"+name);
+    }
+}
