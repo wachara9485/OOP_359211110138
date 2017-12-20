786d61d541f85223cbad4f67c0e81e4dd0fe51c7
LAB_COM-MT_1 <wachara9485@gmail.com>
2017-12-20 16:22:29 +0700

    TestNestedLoop

--- src/oop_lab/s359211110138/rmutsv/com/ooplab2/TestInput.java
+++ src/oop_lab/s359211110138/rmutsv/com/ooplab2/TestInput.java
@@ -3,13 +3,12 @@ package oop_lab.s359211110138.rmutsv.com.ooplab2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-public class TestInput {
-    import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-    public class TestInput {
+public class TestInput {
+
         public static void main(String[] args) throws IOException {
 
 //        BufferedReader
@@ -18,6 +17,7 @@ import java.io.InputStreamReader;
 
             System.out.println("What is your name? :");
             String name = reader.readLine();
-            System.out.println("Your name is"+name);
+            System.out.println("Your name is" + name);
 
         }
+    }
